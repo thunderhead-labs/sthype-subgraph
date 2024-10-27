55a101def7b05a6c1f27cc5c30c3c1eb558f1bb3
tom <tom@thunderhead.xyz>
2024-10-27 10:39:18 +0200

    Initialize subgraph

---
+++ stHYPE-subgraph/src/st-hype.ts
@@ -0,0 +1,273 @@
+import {
+  Approval as ApprovalEvent,
+  Burn as BurnEvent,
+  DefaultAdminDelayChangeCanceled as DefaultAdminDelayChangeCanceledEvent,
+  DefaultAdminDelayChangeScheduled as DefaultAdminDelayChangeScheduledEvent,
+  DefaultAdminTransferCanceled as DefaultAdminTransferCanceledEvent,
+  DefaultAdminTransferScheduled as DefaultAdminTransferScheduledEvent,
+  DelegateChanged as DelegateChangedEvent,
+  DelegateVotesChanged as DelegateVotesChangedEvent,
+  EIP712DomainChanged as EIP712DomainChangedEvent,
+  Initialized as InitializedEvent,
+  Mint as MintEvent,
+  Rebase as RebaseEvent,
+  RoleAdminChanged as RoleAdminChangedEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent,
+  Transfer as TransferEvent
+} from "../generated/stHYPE/stHYPE"
+import {
+  Approval,
+  Burn,
+  DefaultAdminDelayChangeCanceled,
+  DefaultAdminDelayChangeScheduled,
+  DefaultAdminTransferCanceled,
+  DefaultAdminTransferScheduled,
+  DelegateChanged,
+  DelegateVotesChanged,
+  EIP712DomainChanged,
+  Initialized,
+  Mint,
+  Rebase,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBurn(event: BurnEvent): void {
+  let entity = new Burn(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.to = event.params.to
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDefaultAdminDelayChangeCanceled(
+  event: DefaultAdminDelayChangeCanceledEvent
+): void {
+  let entity = new DefaultAdminDelayChangeCanceled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDefaultAdminDelayChangeScheduled(
+  event: DefaultAdminDelayChangeScheduledEvent
+): void {
+  let entity = new DefaultAdminDelayChangeScheduled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.newDelay = event.params.newDelay
+  entity.effectSchedule = event.params.effectSchedule
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDefaultAdminTransferCanceled(
+  event: DefaultAdminTransferCanceledEvent
+): void {
+  let entity = new DefaultAdminTransferCanceled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDefaultAdminTransferScheduled(
+  event: DefaultAdminTransferScheduledEvent
+): void {
+  let entity = new DefaultAdminTransferScheduled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.newAdmin = event.params.newAdmin
+  entity.acceptSchedule = event.params.acceptSchedule
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDelegateChanged(event: DelegateChangedEvent): void {
+  let entity = new DelegateChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.delegator = event.params.delegator
+  entity.fromDelegate = event.params.fromDelegate
+  entity.toDelegate = event.params.toDelegate
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDelegateVotesChanged(
+  event: DelegateVotesChangedEvent
+): void {
+  let entity = new DelegateVotesChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.delegate = event.params.delegate
+  entity.previousVotes = event.params.previousVotes
+  entity.newVotes = event.params.newVotes
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleEIP712DomainChanged(
+  event: EIP712DomainChangedEvent
+): void {
+  let entity = new EIP712DomainChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMint(event: MintEvent): void {
+  let entity = new Mint(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.to = event.params.to
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRebase(event: RebaseEvent): void {
+  let entity = new Rebase(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.currentSupply = event.params.currentSupply
+  entity.newSupply = event.params.newSupply
+  entity.rebaseInterval = event.params.rebaseInterval
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
+  let entity = new RoleAdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.previousAdminRole = event.params.previousAdminRole
+  entity.newAdminRole = event.params.newAdminRole
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleGranted(event: RoleGrantedEvent): void {
+  let entity = new RoleGranted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleRevoked(event: RoleRevokedEvent): void {
+  let entity = new RoleRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
